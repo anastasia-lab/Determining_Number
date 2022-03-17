d01d41e548ed5c95770396c15bd90574e27857a9
anastasia-lab <anastasiya.shpilevaia@ictis.sfedu.ru>
2022-03-17 22:27:45 +0300

    Добавьте файлы проекта.

---
+++ DeterminingNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeterminingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определение чётности числа
+            Console.WriteLine("Здравствуйте!");
+            Console.WriteLine("Введите любое положительное целое число, чтобы определить чётное оно или нет:");
+            int UserNumber = int.Parse(Console.ReadLine());
+
+            if (UserNumber % 2 == 0)
+            {
+                Console.WriteLine("Ваше число четное");
+            }
+            else
+            {
+                Console.WriteLine("Ваше число не четное");
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
